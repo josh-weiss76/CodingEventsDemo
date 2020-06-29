13e7528e4976eac2b0134916f8e6880d88852352
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-29 14:51:43 -0400

    added error checking code for adding new events

---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage ="Event name is required")]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="Event name must be between 3 and 50 characters long")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Event description is required")]
+        [StringLength(500, ErrorMessage ="Description cannot exceed 500 characters")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+    }
+}
