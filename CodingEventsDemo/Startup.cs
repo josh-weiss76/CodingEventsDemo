0929d1e99233bd43bfc7b3678a7dc3e6e68f3df5
Joshua Weiss <sgtkorea9@yahoo.com>
2020-07-08 08:59:32 -0400

    added EventDbContest.cs and updated the Event class and startup.cs files

--- CodingEventsDemo/Startup.cs
+++ CodingEventsDemo/Startup.cs
@@ -24,6 +24,8 @@ namespace CodingEventsDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<EventDbContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
