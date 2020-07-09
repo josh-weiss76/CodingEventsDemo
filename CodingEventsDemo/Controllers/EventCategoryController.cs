2296f8cbbf1896c2f055d10e9a5b1e9ffaa14991
Joshua Weiss <sgtkorea9@yahoo.com>
2020-07-09 11:48:49 -0400

    added code to creat an event, store the event in a database, and list the stored events

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //GET: /<controller>/
+        [Route("/EventCategory/Index")]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.EventCategories.ToList();
+
+            return View(categories);
+        }
+
+        [Route("/EventCategory/Create")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory/Index");
+            }
+            return View("Create", addEventCategoryViewModel);
+        }
+    }
+}
